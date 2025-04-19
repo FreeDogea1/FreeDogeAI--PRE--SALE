ee6cdff7262c49ba6ae9887dfb4030a369048187
FreeDogea1 <freedogeai@gmail.com>
2025-04-19 08:09:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("connectWallet").onclick = function () {
+  alert("Wallet connection is not active yet.");
+};
+
+document.getElementById("buyNow").onclick = function () {
+  alert("Token purchase will start soon!");
+};
